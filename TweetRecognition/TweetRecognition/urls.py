fc6f52e2f05e1942cbbe4b9caad9a61223748ec2
mforman000 <matthew.forman@ucalgary.ca>
2024-03-27 16:49:43 -0600

    Finalized progress report and dynamic pie chart added to webpage

--- TweetRecognition/TweetRecognition/urls.py
+++ TweetRecognition/TweetRecognition/urls.py
@@ -21,4 +21,5 @@ from TweetRecognitionApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('pie_chart_data/', views.pie_chart_data, name='pie_chart_data'),
 ]
--- TweetRecognition/TweetRecognitionApp/views.py
+++ TweetRecognition/TweetRecognitionApp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Topic
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
     topics = Topic.objects.all()
     return render(request, 'index.html', {'topics' : topics})
+
+def pie_chart_data(request):
+    data = {
+        'labels' : ["positive", "Negative", "Neutral"],
+        'data' : [10,20,30]
+    }
+    return(JsonResponse(data))
