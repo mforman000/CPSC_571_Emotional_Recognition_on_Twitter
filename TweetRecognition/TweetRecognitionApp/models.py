2ac10226882b9f6b8721a24ea1e33e6ec585f532
mforman000 <matthew.forman@ucalgary.ca>
2024-04-07 12:14:18 -0600

    Pie chart dynamically changes based off values associated with each topic

--- TweetRecognition/TweetRecognitionApp/models.py
+++ TweetRecognition/TweetRecognitionApp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Topic(models.Model):
-    name = models.CharField(max_length=100)
+    topic = models.CharField(max_length=100)
+    value = models.JSONField()
 
     def __str__(self):
-        return self.name
+        return self.topic
